5ed488bedc2e90959327c329df28f3d08aa5d38d
QwertyCoolMT <erichd@gmail.com>
2021-12-09 21:33:43 -0500

    question 1

---
+++ REACT Q1.js
@@ -0,0 +1,31 @@
+
+import keyIndex from 'react-key-index';
+import { View } from 'react-native';
+
+const rooms = [
+    { room_type: "Queen", vacant_rooms: 5, price: 100 },
+    { room_type: "Double", vacant_rooms: 3, price: 75 },
+    { room_type: "Twin", vacant_rooms: 8, price: 60 }
+  ];
+  
+  //allows us to create list items with unique IDs for each
+  const rooms_with_ids = keyIndex(rooms, 1)
+  
+  function available_rooms(props){
+    return(
+      <ol>
+        {props.data.map((room) => 
+        <li key={room._room_typeId}> {`${room.room_type}, ${room.vacant_rooms}, ${room.price}`} </li> )}
+      </ol>
+  )
+  };
+
+  export default function App() {
+    return (
+      <View style={styles.container}>
+        <available_rooms data={rooms_with_ids} />  
+      </View>
+      
+  );
+    
+  }
